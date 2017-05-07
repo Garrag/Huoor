a4c6294f0b1165a244edc458360abae463eed905
Garrag <Garrag_jun@163.com>
2017-05-07 10:21:45 +0800

    修改端口,增加更新功能

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/admin', function(req, res, next) {
+  res.render('index');
+});
+
+//拉取github代码
+router.all('/updateGit', function(req, res, next) {
+	console.log('start --------------updateGit------------------')
+	var cmd ='cd /home/git/ChessGame/ && git pull origin master';   //'cd ~ && ls' 
+	exec(cmd, function(err, stdout, stderr){
+		if(err){
+			res.jsonp({code:1, msg:stderr})
+			console.log(stderr);
+			return;
+		}else {
+			res.jsonp({code:0, msg:stdout})
+			console.log(stdout)
+			console.log('end --------------updateGit------------------')
+		}
+	})
 });
 
 module.exports = router;
