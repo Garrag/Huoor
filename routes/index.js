24dfbe160c715abefb31b68678ecf07792a42918
Dall <347891760@qq.com>
2019-11-27 11:36:49 +0800

    修复更新功能

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+	res.render('index');
 });
 
-router.get('/admin', function(req, res, next) {
-  res.render('admin');
+router.get('/admin', function (req, res, next) {
+	res.render('admin');
 });
 
 //拉取github代码
-router.all('/updateGit', function(req, res, next) {
+router.all('/update', function (req, res, next) {
 	console.log('start --------------updateGit------------------')
-	var cmd ='cd /home/git/ChessGame/ && git pull origin master';   //'cd ~ && ls' 
-	exec(cmd, function(err, stdout, stderr){
-		if(err){
-			res.jsonp({code:1, msg:stderr})
+	var cmd = 'cd /Huoor && git pull';   //'cd ~ && ls' 
+	exec(cmd, function (err, stdout, stderr) {
+		if (err) {
+			res.jsonp({ code: 1, msg: stderr })
 			console.log(stderr);
 			return;
-		}else {
-			res.jsonp({code:0, msg:stdout})
+		} else {
+			res.jsonp({ code: 0, msg: stdout })
 			console.log(stdout)
 			console.log('end --------------updateGit------------------')
 		}
